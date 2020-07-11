0b29c4efe8909f2862a864252fd7e5dea19112eb
Brian Schreder <bschreder@yahoo.com>
2020-07-11 16:46:02 -0400

    update directory structure

--- src/PlacesApp/App_Start/BundleConfig.cs
+++ src/PlacesApp/App_Start/BundleConfig.cs
@@ -9,22 +9,22 @@ namespace PlacesApp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/UI/Scripts/jquery-{version}.js"));
+                        "~/ViewResources/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/UI/Scripts/jquery.validate*"));
+                        "~/ViewResources/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/UI/Scripts/modernizr-*"));
+                        "~/ViewResources/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/UI/Scripts/bootstrap.js"));
+                      "~/ViewResources/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/UI/Content/bootstrap.css",
-                      "~/UI/Content/site.css"));
+                      "~/ViewResources/Content/bootstrap.css",
+                      "~/ViewResources/Content/site.css"));
         }
     }
 }
